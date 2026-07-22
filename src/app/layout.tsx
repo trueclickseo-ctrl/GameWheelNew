367bcbe49e73a16d4fd425cfcd553f8ca3409535
GameWheel Developer <developer@gamewheelclub.com>
2026-07-22 11:32:08 +0500

    chore: save source branch progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,17 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PSHCQN2P');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -53,6 +64,16 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col bg-cream text-retro-navy dark:bg-retro-navy dark:text-cream">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSHCQN2P"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
